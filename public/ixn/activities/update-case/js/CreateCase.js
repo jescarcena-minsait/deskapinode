edb02ba80baf8a127652b476e626d9030cce31a9
jescarcena-minsait <jescarcena@minsait.com>
2017-05-30 13:12:14 +0200

    END

--- public/ixn/activities/update-case/js/CreateCase.js
+++ public/ixn/activities/update-case/js/CreateCase.js
@@ -28,13 +28,20 @@ define( function( require ) {
 					oArgs[key] = aArgs[i][key]; 
 				}
 			}
-			//see createCase for an example on how to use config.json data during activity configuration.            
+			//oArgs.priority will contain a value if this activity has already been configured:
+			priority = oArgs.priority || toJbPayload['configurationArguments'].defaults.priority;            
         }
         
 		$.get( "/version", function( data ) {
 			$('#version').html('Version: ' + data.version);
 		});                
-	
+
+        // If there is no priority selected, disable the next button
+        if (!priority) {
+            connection.trigger('updateButton', { button: 'next', enabled: false });
+        }
+
+		$('#selectPriority').find('option[value='+ priority +']').attr('selected', 'selected');		
 		gotoStep(step);
         
     });
@@ -73,7 +80,11 @@ define( function( require ) {
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
 
-        //setup UI events here.
+        // Disable the next button if a value isn't selected
+        $('#selectPriority').change(function() {
+            var priority = getPriority();
+            connection.trigger('updateButton', { button: 'next', enabled: Boolean(priority) });
+        });
     };
 
     function gotoStep(step) {
@@ -81,18 +92,43 @@ define( function( require ) {
         switch(step) {
             case 1:
                 $('#step1').show();
+                connection.trigger('updateButton', { button: 'next', text: 'next', enabled: Boolean(getPriority()) });
                 connection.trigger('updateButton', { button: 'back', visible: false });
+                break;
+            case 2:
+                $('#step2').show();
+                $('#showPriority').html(getPriority());
+                connection.trigger('updateButton', { button: 'back', visible: true });
                 connection.trigger('updateButton', { button: 'next', text: 'done', visible: true });
                 break;
-            case 2: // Only 2 steps, so the equivalent of 'done' - send off the payload
+            case 3: // Only 2 steps, so the equivalent of 'done' - send off the payload
                 save();
                 break;
         }
     };
 
+    function getPriority() {
+        return $('#selectPriority').find('option:selected').attr('value').trim();
+    };
+
     function save() {
 
-		//inArguments key:value pairs can be set here to be used by the first custom activity...see createCase.
+        var value = getPriority();
+
+        // toJbPayload is initialized on populateFields above.  Journey Builder sends an initial payload with defaults
+        // set by this activity's config.json file.  Any property may be overridden as desired.
+        //toJbPayload.name = "my activity";
+
+		//this will be sent into the custom activity body within the inArguments array.
+        toJbPayload['arguments'].execute.inArguments.push({"priority": value});
+
+		/*
+        toJbPayload['metaData'].things = 'stuff';
+        toJbPayload['metaData'].icon = 'path/to/icon/set/from/iframe/icon.png';
+        toJbPayload['configurationArguments'].version = '1.1'; // optional - for 3rd party to track their customActivity.js version
+        toJbPayload['configurationArguments'].partnerActivityId = '49198498';
+        toJbPayload['configurationArguments'].myConfiguration = 'configuration coming from iframe';
+		*/
 		
 		toJbPayload.metaData.isConfigured = true;  //this is required by JB to set the activity as Configured.
         connection.trigger('updateActivity', toJbPayload);
