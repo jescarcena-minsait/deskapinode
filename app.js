8128f83bc397e4c599cd7f39e0958ab0005a7b5f
jescarcena-minsait <jescarcena@minsait.com>
2017-06-01 13:36:42 +0200

    END

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var activityCreate   = require('./routes/activityCreate');
 var activityUpdate   = require('./routes/activityUpdate');
 var activityUtils    = require('./routes/activityUtils');
 
-var routes_jescarcena      = require('./routes_jescarcena');
-var createFile   = require('./routes_jescarcena/createFile');
+//var routes_jescarcena      = require('./routes_jescarcena');
+//var createFile   = require('./routes_jescarcena/createFile');
 
 var pkgjson = require( './package.json' );
 
@@ -74,7 +74,7 @@ if ('development' == app.get('env')) {
 
 // HubExchange Routes
 app.get('/', routes.index );
-app.get('/', routes_jescarcena.index );
+//app.get('/', routes_jescarcena.index );
 app.post('/login', tokenFromJWT, routes.login );
 app.post('/logout', routes.logout );
 
